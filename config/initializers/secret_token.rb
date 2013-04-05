93e0bd6c391359179573a281b125e383644054c4
your name <huyen@huyen-nguyen.(none)>
2013-04-05 09:36:28 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaiTest::Application.config.secret_token = '74da620344392daad33695e640b5e594064306dbd6a7c481dcc9ae2980fd14ca81dc4cb29e4df9a31964efe7d7dd9eda2500969cd2856570047010a200bee531'
